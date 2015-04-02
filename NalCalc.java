108fd738d5cd19f474fe279e3cafd67f4daaffbd
Adelik <detta5@mail.ru>
2015-04-02 20:58:15 +0500

    Менеджер пользовательского интерфейса

--- NalCalc.java
+++ NalCalc.java
@@ -172,7 +172,22 @@ public class NalogCalc extends javax.swing.JFrame {
         jTextField3.setText(String.valueOf(sum - tax));
         jTextField4.setText(String.valueOf(tax));
         jTextField5.setText(String.valueOf(sum));
-    }                                        
+    }
+    * @param args the command line arguments
+     */
+    public static void main(String args[]) {
+        /* Set the Nimbus look and feel */
+        //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
+        /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
+         * For details see http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html 
+         */
+        try {
+            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
+                if ("Nimbus".equals(info.getName())) {
+                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
+                    break;
+                }
+            }
 
        
     // Variables declaration - do not modify                     
