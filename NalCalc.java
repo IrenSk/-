ff1a26438a58bb7b10301ea2844a67bb4e084afa
Ku4erov <art_k14@mail.ru>
2015-04-02 21:01:52 +0500

    Мелкие правки и вывод экранной формы

--- NalCalc.java
+++ NalCalc.java
@@ -188,6 +188,23 @@ public class NalogCalc extends javax.swing.JFrame {
                     break;
                 }
             }
+        } catch (ClassNotFoundException ex) {
+            java.util.logging.Logger.getLogger(NalogCalc.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (InstantiationException ex) {
+            java.util.logging.Logger.getLogger(NalogCalc.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (IllegalAccessException ex) {            java.util.logging.Logger.getLogger(NalogCalc.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (javax.swing.UnsupportedLookAndFeelException ex) {
+            java.util.logging.Logger.getLogger(NalogCalc.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        }
+        //</editor-fold>
+
+        /* Create and display the form */
+        java.awt.EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                new NalogCalc().setVisible(true);
+            }
+        });
+    }
 
        
     // Variables declaration - do not modify                     
